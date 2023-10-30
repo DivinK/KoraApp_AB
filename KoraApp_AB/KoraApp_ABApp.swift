2682966012bf58821552cdc0ef8f80fd424490bc
DivinK <kdivin@icloud.com>
2023-10-30 04:33:35 +0100

    Initial Commit

---
+++ KoraApp_AB/KoraApp_ABApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoraApp_ABApp.swift
+//  KoraApp_AB
+//
+//  Created by Divin Karagira on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct KoraApp_ABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
